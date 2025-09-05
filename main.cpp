2d9a6e561561a8980b4e2f4b61295ebd9ae3e0cf
MPete2002 <peter.marino@student.northampton.edu>
2025-09-05 01:01:21 +0000

    Calculated Area and Perimeter

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  const double length = 7.2;
-  const double width = 1.3;
+int main() 
 
+{
 
+  // Formulas
+
+double length = 7.2;
+double width = 1.3;
+double perimeter = 2 * (length + width);
+double area = length * width;
+
+// Calculations
+
+cout << "The perimeter is equivalent to " << perimeter << endl; 
+cout << "The area is equivalent to " << area << endl;
 }
